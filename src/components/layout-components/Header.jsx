99d4cd64a03bbba5d91c90c8b7ddade0b2b24698
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2025-06-25 12:05:31 +0200

    first commit

---
+++ src/components/layout-components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../../assets/logoe.png";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="bg-white mx-5 pt-5 rounded-2xl p-5">
+      <div className="flex items-center justify-between">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-35 h-9" />
+        </Link>
+        <div className="flex items-center justify-center gap-5">
+          <h1>Immobilier</h1>
+          <h1>Électroménager</h1>
+          <h1>Véhicules</h1>
+          <h1>Informatique</h1>
+          <h1>Bricolage</h1>
+        </div>
+        <div className="flex items-center justify-center gap-5">
+          <button className="bg-[#346644] text-white px-4 py-2 rounded-md">
+            <Link to="/connexion">Connexion</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/layout-components/Header";
+
+function Home() {
+  return (
+    <div className="bg-[#0F2E19] h-screen pt-5">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
