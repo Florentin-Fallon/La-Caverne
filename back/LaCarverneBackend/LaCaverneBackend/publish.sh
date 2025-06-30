e1b83d1c55b7e09670abbec72505bf63452d7a16
Cacahuète Dev <24205659+CacahueteSansSel@users.noreply.github.com>
2025-06-30 09:33:10 +0200

    backend: added a way to migrate the database directly using command line arguments

--- back/LaCarverneBackend/LaCaverneBackend/publish.sh
+++ back/LaCarverneBackend/LaCaverneBackend/publish.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 dotnet publish -r linux-x64 --sc -p:PublishSingleFile=True -o Output
+ssh vps sudo systemctl stop lacaverne.service
 scp Output/LaCaverneBackend vps:/srv/lacaverne/LaCaverneBackend
+ssh vps sudo systemctl enable --now lacaverne.service
