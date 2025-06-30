44b022f9648491175032d5fef5eb593f107fb075
Cacahuète Dev <24205659+CacahueteSansSel@users.noreply.github.com>
2025-06-30 10:03:06 +0200

    backend: added admin permissions to users

---
+++ back/LaCarverneBackend/LaCaverneBackend/Migrations/20250630073921_UserAdmins.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LaCaverneBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAdmins : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Accounts",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Accounts");
+        }
+    }
+}
